2ed1939e19d770fa0c98de3258984a5dbe1fe1b4
bukomagoda <bhasithaudara1b@gmail.com>
2022-04-08 16:12:16 +0530

    Update settings.py

--- img2text/settings.py
+++ img2text/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS =  [ BASE_DIR / 'static']
 
